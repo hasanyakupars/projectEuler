bb6833913b84f2e36f5b6ff47e815d04a5bbd16a
yakuğ <133968563+hasanyakupars@users.noreply.github.com>
2023-07-28 13:44:52 +0300

    Add files via upload

---
+++ problem9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// Find the only Pythagorean triplet (a, b, c), for which a + b + c = 1000.
+
+int main()
+{
+    int a,b,csq,total=1000;
+    for(a=1; a<total; a++){
+        for(b=1; b<total/2; b++){
+            csq = a*a + b*b;
+            if(a + b + sqrt(csq) == total)
+                goto theEnd;
+        }
+    }
+    theEnd:
+    int c = sqrt(csq);
+    cout << " a: " << a << "\n b: " << b << "\n c: " << c << "\n mult: " << a*b*c;
+    return 0;
+}
