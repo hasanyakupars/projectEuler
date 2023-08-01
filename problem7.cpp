51ba98a7396925a85fcbe4f0910f11529f38b9df
yakuğ <133968563+hasanyakupars@users.noreply.github.com>
2023-08-01 23:17:43 +0300

    Update problem7.cpp

--- problem7.cpp
+++ problem7.cpp
@@ -3,8 +3,8 @@
 #include <vector>
 using namespace std;
 
-bool isPrimal(int number){
-    if(number % 2 == 0) // even numbers can't be primal.
+bool isPrime(int number){
+    if(number % 2 == 0) // even numbers can't be prime.
         return 0;
 
     int temp = sqrt(number);    // square root of number rounded to the greatest integer.
@@ -19,21 +19,22 @@ int main()
 {
     vector<int> vector;
     int i=1, number;
-    vector.push_back(2);    // already know 2 and 3 are primals.
+    vector.push_back(2);    // already know 2 and 3 are prime.
     vector.push_back(3);
 
     while(vector.size() != 10001){
-        number = 6*i - 1;   // all primal numbers are either 6n - 1 or 6n + 1.
-        if(isPrimal(number))
+        number = 6*i - 1;   // all prime numbers are either 6n - 1 or 6n + 1.
+        if(isPrime(number))
             vector.push_back(number);
 
         number = 6*i + 1;
-        if(isPrimal(number))
+        if(isPrime(number))
             vector.push_back(number);
 
         i++;
     } 
     cout << vector[10000];
+//    Print all prime numbers until 10000.
 //    int index = 0;
 //    for(auto it = vector.begin(); it != vector.end(); it++)
 //         cout << index++ << " - "<< *it << endl;
